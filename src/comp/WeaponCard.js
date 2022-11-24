a3c31426bdede36b9216e1a710436a103e01916b
vsc <valentin.schiffl@agenda-software.de>
2022-11-24 08:04:17 +0100

    🐛Fixed Bleeding In WeaponCard

--- src/comp/WeaponCard.js
+++ src/comp/WeaponCard.js
@@ -12,7 +12,7 @@ function WeaponCard(params) {
     }
 
     return(                
-        <div className="min-w-[300px] max-w-[380px] flex-grow bg-background-very-dark border-2 border-current-line transition-all">
+        <div className="min-w-[300px] max-w-[380px] flex-grow bg-current-line border-2 border-current-line transition-all flex flex-col">
             <div className="bg-current-line p-2">
                 <h2 className="text-foreground-highlight text-2xl">{params.weapon.name}</h2> 
             </div>
@@ -27,7 +27,7 @@ function WeaponCard(params) {
                 </h2>
                 <ReactSVG src={require("../res/wpn_grps/weapon-group-" + params.weapon.weaponGroup + ".svg")} className='fill-red absolute h-36 w-36 -right-4 -bottom-8'/>
             </div>
-            <div className="flex flex-col p-2 w-full">
+            <div className="flex flex-col p-2 w-full bg-background-very-dark flex-grow">
                 <div className="grid grid-cols-2 grid-flow-row-dense justify-between w-full">
                     <h3>Initiative</h3>
                     <h2 className="text-right"><span className="text-cyan">({getPrefix(params.weapon.coreInitiative)})</span> {getPrefix(params.weapon.initiative)}</h2>
@@ -36,8 +36,7 @@ function WeaponCard(params) {
                     <h3>Angriffsbonus</h3>
                     <h2 className="text-right">{getPrefix(params.weapon.attackBonus)}</h2>
                     <h3>Defensivbonus</h3>
-                    <h2 className="text-right">{getPrefix(params.weapon.defenseBonus)}</h2>
-                    
+                    <h2 className="text-right">{getPrefix(params.weapon.defenseBonus)}</h2>   
                 </div>
                 <hr className=" border-current-line my-2"/>
                 <h3 className="italic">
