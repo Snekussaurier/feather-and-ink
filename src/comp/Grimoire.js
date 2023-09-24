99e33ba4bdd1d48fcc6cc7eaeb1f2eacc0fb9ef2
Snekussaurier <valentin.schiffl@gmail.com>
2023-09-24 21:24:11 +0200

    Prepared branch

--- src/comp/Grimoire.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Grimoire() {
-  return (
-    <div className="flex flex-col gap-4 h-fit pt-24 pb-12 px-8 max-w-[1160px] min-w-[910px] w-full z-10">
-        <h1 className="text-5xl">Grimoire</h1>
-    </div>
-  )
-}
--- src/comp/Tag.js
+++
@@ -1,9 +0,0 @@
-function Tag(params) {
-    return(
-        <div className="p-[2px] rounded-sm bg-background w-fit">
-            <p className="text-foreground px-2 py-1">{params.text}</p>
-        </div>
-    )
-};
-
-export default Tag;
